fd3e69f65b0a5a235e05b00b84f767d9e996158b
Wilson Berkow <wilsonberkow@gmail.com>
2016-03-21 19:26:48 -0400

    Setup timeouts and retries for robustness

--- src/edu/stuy/util/TegraSocketReader.java
+++ src/edu/stuy/util/TegraSocketReader.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ConnectException;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicReference;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
@@ -30,7 +33,7 @@ public class TegraSocketReader implements Runnable {
     private boolean connected;
 
     // tegraHost IP determined by port used on the radio
-    private static final String expectedTegraIP = "10.6.94.19";
+    private static final String tegraIP = "10.6.94.19";
     private static final int tegraPort = 7123;
 
     public TegraSocketReader() {
@@ -44,7 +47,7 @@ public class TegraSocketReader implements Runnable {
      * <code>tegraPort</code>
      */
     private void setupSocket() {
-        setupSocketAt(expectedTegraIP);
+        setupSocketAt(tegraIP);
     }
 
     /**
@@ -54,16 +57,26 @@ public class TegraSocketReader implements Runnable {
      */
     private void setupSocketAt(String ip) {
         try {
+            System.out.println("About to create new Socket at " + ip + ":" + tegraPort);
+
             connected = false;
-            tegra = new Socket(ip, tegraPort);
-            if (tegra == null) return;
+            // If the following throws, `connected` remains false
+
+            // Connect to the Tegra
+            tegra = new Socket();
+            tegra.connect(new InetSocketAddress(ip, tegraPort), 1000);
+
+            // Set a timeout on Socket IO for when we read data
+            tegra.setSoTimeout(1000);
+
             connected = true;
-            SmartDashboard.putString("ip connected to tegra by", ip);
         } catch (ConnectException e) {
-            System.err.println("Failed to connect to " + expectedTegraIP + ":" + tegraPort);
+            System.err.println("Failed to connect to " + tegraIP + ":" + tegraPort);
             System.err.println("Exception was: " + e + "\n\n");
         } catch (UnknownHostException e) {
-            System.err.println("Could not resolve host at " + expectedTegraIP + ":" + tegraPort);
+            System.err.println("Could not resolve host at " + tegraIP + ":" + tegraPort);
+        } catch (SocketTimeoutException e) {
+            System.err.println("Connection to " + ip + ":" + tegraPort + " timed out.");
         } catch (IOException e) {
             System.err.println("An IOException has occurred. Message: " + e.getMessage());
         } catch (Exception e) {
@@ -85,14 +98,15 @@ public class TegraSocketReader implements Runnable {
         // The following while loop is for trying again and again when
         // connection fails, to make this robust to situations in which
         // the Tegra restarts or has not yet started.
-        while (true) {
+        while (!isInterrupted()) {
             try {
                 System.out.println("About to open input stream from Tegra socket");
                 // Open input stream and read data until thread is interrupted
                 InputStreamReader inStream = new InputStreamReader(tegra.getInputStream());
                 BufferedReader bufIn = new BufferedReader(inStream);
                 try {
-                    while (!Thread.currentThread().isInterrupted()) {
+                    while (!isInterrupted()) {
+                        // The .readLine() call will timeout after time specified in setupSocketAt
                         double[] msg = parseMessage(bufIn.readLine());
                         latestData.set(msg);
                         System.out.println("Read data from Tegra: " + Arrays.toString(msg));
@@ -102,20 +116,20 @@ public class TegraSocketReader implements Runnable {
                     inStream.close();
                     System.out.println("Closed bufIn and inStream");
                 }
-                break;
             } catch (SocketException e) {
                 // Occurs when the server shuts down (or resets)
-                System.out.println("Will try to reconnect");
+                System.out.println("Caught: " + e.getMessage() + " Will try to reconnect");
                 setupSocket();
                 // Create new socket and, via the while loop, try again
             } catch (IOException e) {
-                System.out.println("Will try to reconnect");
+                System.out.println("Caught: " + e.getMessage() + " Will try to reconnect");
                 setupSocket();
             } catch (Exception e) {
                 System.out.println("General exception caught in loop of run(): " + e);
                 setupSocket();
             }
         }
+        System.out.println("At end of run(): finishing Tegra thread");
     }
 
     /**
@@ -159,6 +173,13 @@ public class TegraSocketReader implements Runnable {
      */
     public boolean isConnected() {
         return connected;
+    }
+
+    private boolean isInterrupted() {
+        // Call isInterrupted on currentThread, not Thread.interrupted(),
+        // as the latter *resets* the interrupted status and, when called
+        // twice in a row, will likely return false: an *unwanted* side-effect
+        return Thread.currentThread().isInterrupted();
     }
 
     // Only run during testing
